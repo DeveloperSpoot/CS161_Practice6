a7c82fd267b5f3db7cecffa9f4b58d3bf40951df
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2024-02-03 01:31:23 -0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace CS161_Practice6
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            stepOutCmdButton = new Button();
+            stepOverCmdButton = new Button();
+            stepIntoCmdButton = new Button();
+            valueReturningButton = new Button();
+            outputPraramButton = new Button();
+            passingArgumentMethod = new Button();
+            creatingCallingMethodsButton = new Button();
+            groupBox2 = new GroupBox();
+            joesAutoButton = new Button();
+            kineticEnergyButton = new Button();
+            button1 = new Button();
+            retailPriceCalcButton = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(stepOutCmdButton);
+            groupBox1.Controls.Add(stepOverCmdButton);
+            groupBox1.Controls.Add(stepIntoCmdButton);
+            groupBox1.Controls.Add(valueReturningButton);
+            groupBox1.Controls.Add(outputPraramButton);
+            groupBox1.Controls.Add(passingArgumentMethod);
+            groupBox1.Controls.Add(creatingCallingMethodsButton);
+            groupBox1.Location = new Point(11, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(240, 426);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tutorials";
+            // 
+            // stepOutCmdButton
+            // 
+            stepOutCmdButton.Location = new Point(8, 292);
+            stepOutCmdButton.Name = "stepOutCmdButton";
+            stepOutCmdButton.Size = new Size(223, 23);
+            stepOutCmdButton.TabIndex = 6;
+            stepOutCmdButton.Text = "Step Out Cmd";
+            stepOutCmdButton.UseVisualStyleBackColor = true;
+            stepOutCmdButton.Click += stepOutCmdButton_Click;
+            // 
+            // stepOverCmdButton
+            // 
+            stepOverCmdButton.Location = new Point(8, 247);
+            stepOverCmdButton.Name = "stepOverCmdButton";
+            stepOverCmdButton.Size = new Size(223, 23);
+            stepOverCmdButton.TabIndex = 5;
+            stepOverCmdButton.Text = "Step Over Cmd";
+            stepOverCmdButton.UseVisualStyleBackColor = true;
+            stepOverCmdButton.Click += stepOverCmdButton_Click;
+            // 
+            // stepIntoCmdButton
+            // 
+            stepIntoCmdButton.Location = new Point(9, 202);
+            stepIntoCmdButton.Name = "stepIntoCmdButton";
+            stepIntoCmdButton.Size = new Size(223, 23);
+            stepIntoCmdButton.TabIndex = 4;
+            stepIntoCmdButton.Text = "Step Into Cmd";
+            stepIntoCmdButton.UseVisualStyleBackColor = true;
+            stepIntoCmdButton.Click += stepIntoCmdButton_Click;
+            // 
+            // valueReturningButton
+            // 
+            valueReturningButton.Location = new Point(8, 157);
+            valueReturningButton.Name = "valueReturningButton";
+            valueReturningButton.Size = new Size(223, 23);
+            valueReturningButton.TabIndex = 3;
+            valueReturningButton.Text = "Returning Value Mathod";
+            valueReturningButton.UseVisualStyleBackColor = true;
+            valueReturningButton.Click += valueReturningButton_Click;
+            // 
+            // outputPraramButton
+            // 
+            outputPraramButton.Location = new Point(8, 112);
+            outputPraramButton.Name = "outputPraramButton";
+            outputPraramButton.Size = new Size(223, 23);
+            outputPraramButton.TabIndex = 2;
+            outputPraramButton.Text = "Using Output Param";
+            outputPraramButton.UseVisualStyleBackColor = true;
+            outputPraramButton.Click += outputPraramButton_Click;
+            // 
+            // passingArgumentMethod
+            // 
+            passingArgumentMethod.Location = new Point(8, 67);
+            passingArgumentMethod.Name = "passingArgumentMethod";
+            passingArgumentMethod.Size = new Size(223, 23);
+            passingArgumentMethod.TabIndex = 1;
+            passingArgumentMethod.Text = "Passing an Argument";
+            passingArgumentMethod.UseVisualStyleBackColor = true;
+            passingArgumentMethod.Click += passingArgumentMethod_Click;
+            // 
+            // creatingCallingMethodsButton
+            // 
+            creatingCallingMethodsButton.Location = new Point(8, 22);
+            creatingCallingMethodsButton.Name = "creatingCallingMethodsButton";
+            creatingCallingMethodsButton.Size = new Size(223, 23);
+            creatingCallingMethodsButton.TabIndex = 0;
+            creatingCallingMethodsButton.Text = "Createing + Calling Methods";
+            creatingCallingMethodsButton.UseVisualStyleBackColor = true;
+            creatingCallingMethodsButton.Click += creatingCallingMethodsButton_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(joesAutoButton);
+            groupBox2.Controls.Add(kineticEnergyButton);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(retailPriceCalcButton);
+            groupBox2.Location = new Point(272, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(240, 426);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Programming Problem";
+            // 
+            // joesAutoButton
+            // 
+            joesAutoButton.Location = new Point(9, 157);
+            joesAutoButton.Name = "joesAutoButton";
+            joesAutoButton.Size = new Size(223, 23);
+            joesAutoButton.TabIndex = 4;
+            joesAutoButton.Text = "Joe's Automotive";
+            joesAutoButton.UseVisualStyleBackColor = true;
+            joesAutoButton.Click += joesAutoButton_Click;
+            // 
+            // kineticEnergyButton
+            // 
+            kineticEnergyButton.Location = new Point(9, 112);
+            kineticEnergyButton.Name = "kineticEnergyButton";
+            kineticEnergyButton.Size = new Size(223, 23);
+            kineticEnergyButton.TabIndex = 3;
+            kineticEnergyButton.Text = "Kinetic Energy";
+            kineticEnergyButton.UseVisualStyleBackColor = true;
+            kineticEnergyButton.Click += kineticEnergyButton_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(9, 67);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Falling Distance";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // retailPriceCalcButton
+            // 
+            retailPriceCalcButton.Location = new Point(9, 21);
+            retailPriceCalcButton.Name = "retailPriceCalcButton";
+            retailPriceCalcButton.Size = new Size(223, 23);
+            retailPriceCalcButton.TabIndex = 1;
+            retailPriceCalcButton.Text = "Retail Price Calculator";
+            retailPriceCalcButton.UseVisualStyleBackColor = true;
+            retailPriceCalcButton.Click += retailPriceCalcButton_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(523, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Main";
+            Text = "Main";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button creatingCallingMethodsButton;
+        private GroupBox groupBox2;
+        private Button stepOutCmdButton;
+        private Button stepOverCmdButton;
+        private Button stepIntoCmdButton;
+        private Button valueReturningButton;
+        private Button outputPraramButton;
+        private Button passingArgumentMethod;
+        private Button retailPriceCalcButton;
+        private Button kineticEnergyButton;
+        private Button button1;
+        private Button joesAutoButton;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,65 @@
+namespace CS161_Practice6
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void creatingCallingMethodsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passingArgumentMethod_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputPraramButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valueReturningButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stepIntoCmdButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stepOverCmdButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stepOutCmdButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void retailPriceCalcButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kineticEnergyButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void joesAutoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
